9993a0ff9c0a4249fd8476dbf366e3429f4d1f14
william may <wmay@hotmail.es>
2021-12-18 02:05:26 -0500

    1sprinten1 ver11

---
+++ app/src/main/java/com/wmay/guiat/ListadoPuntos.kt
@@ -0,0 +1,11 @@
+package com.wmay.guiat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoPuntos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_puntos)
+    }
+}
